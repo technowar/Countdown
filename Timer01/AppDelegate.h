77d9ca799722f32664cffb669abf78e23c250fa4
Johmel Rey Pintor <jahmgwapo@skolpad.org>
2013-03-02 09:35:05 +0800

    Initial commit.

---
+++ Timer01/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Timer01
+//
+//  Created by OSX on 3/1/13.
+//  Copyright (c) 2013 OSX. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
